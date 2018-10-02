c4f3bfac358b7442ad03bf57379a2283975f38ba
Hexchan <hexchan16@gmail.com>
2018-10-02 17:52:07 +0300

    Start page!

--- src/imageboard/views/start_page.py
+++ src/imageboard/views/start_page.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from imageboard.models import Board, Thread, Post
+
 
 def start_page(request):
-    return render(request, 'imageboard/start_page.html', {})
+    # Get boards
+    boards = Board.objects.order_by('hid').filter(is_deleted=False, is_hidden=False)
+
+    return render(
+        request,
+        'imageboard/start_page.html',
+        {
+            'boards': boards,
+        }
+    )
