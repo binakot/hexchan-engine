c0a8d1a40075f23772472953f71da3317b884d2b
Hexchan <hexchan16@gmail.com>
2019-01-26 14:45:58 +0300

    Captcha solution char limit increased

--- src/captcha/models.py
+++ src/captcha/models.py
@@ -5,5 +5,5 @@ class Captcha(models.Model):
     """docstring for Captcha"""
 
     public_id = models.CharField(max_length=32)
-    solution = models.CharField(max_length=8)
+    solution = models.CharField(max_length=32)
     image = models.TextField()
