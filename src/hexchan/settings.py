339859335be4447ddeebe075ea0b7fca82327276
Your Name <you@example.com>
2024-01-20 19:13:15 -0600

    fix: django debug toolbar needs to set an INTERNAL_IPS setting in Docker

--- src/hexchan/settings.py
+++ src/hexchan/settings.py
@@ -218,3 +218,8 @@ LOGGING = {
 FIXTURE_DIRS = [
     str(STORAGE_DIR / 'fixtures')
 ]
+
+if DEBUG:
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
